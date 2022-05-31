1dc2dab13c71db42a2fd02801bfe6136d8ddbbd1
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-05-31 10:42:32 -0300

    MOD - Sketch serial com and servo

--- arduino_test1.ino
+++ arduino_test1.ino
@@ -1,12 +1,20 @@
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 #define TRIG 8
 #define ECHO 9
 #define N_SAMPLES 5
+#define SERVO 6
+#define MAX_DEGREE 180
+#define MULTIPLIER 5
+#define delimeter "|"
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
 float getDistance();
 float measure();
+int degree;
+int direction;
+Servo servo;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,16 +23,30 @@ void setup() {
   lcd.backlight();
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
+  servo.attach(SERVO);
+  servo.write(0);
+  degree = 0;
+  direction = 1;
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  degree = degree + MULTIPLIER*direction;
+  servo.write(degree);
+  if (degree >= MAX_DEGREE)
+      direction = -1;
+  else if (degree <= -MAX_DEGREE)
+      direction = 1;
   float distance = measure();
   lcd.clear();
-  Serial.print(distance);
-  Serial.print("\n");
+  Serial.print(-degree);
+  Serial.print(delimeter);
+  Serial.println((int)distance);
+  //Serial.print();
+  lcd.print(degree);
   lcd.print(distance);
+  
   //delay(100);
 }
 
